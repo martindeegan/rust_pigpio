d8afa622b91617d072a6ad5a011a8f2164f33b8d
Martin Deegan <mddeegan@gmail.com>
2017-06-07 13:21:02 -0700

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,7 @@
 #![crate_name = "rust_pigpio"]
 #![allow(dead_code)]
 
+//! #Rust PiGPIO
+//!
+//! The Rust wrapper of the C library functions
 pub mod pigpio;
--- src/pigpio.rs
+++ src/pigpio.rs
@@ -18,6 +18,7 @@ extern "C" {
 
     fn gpioSetPWMrange(user_gpio: u32, range: u32) -> i32;
     fn gpioGetPWMrange(user_gpio: u32) -> i32;
+    fn gpioGetPWMrealRange(user_gpio: u32) -> i32;
     fn gpioSetPWMfrequency(user_gpio: u32, frequency: u32) -> i32;
     fn gpioGetPWMfrequency(user_gpio: u32) -> i32;
 
@@ -172,7 +173,7 @@ const PI_BAD_SPI_BAUD: i32 = -141;
 const PI_NOT_SPI_GPIO: i32 = -142;
 const PI_BAD_EVENT_ID: i32 = -143;
 
-//Modes
+/// Modes
 pub const PI_INPUT: u32 = 0;
 pub const PI_OUTPUT: u32 = 1;
 pub const ALT0: u32 = 4;
@@ -182,32 +183,34 @@ pub const ALT3: u32 = 7;
 pub const ALT4: u32 = 3;
 pub const ALT5: u32 = 2;
 
-//Values
-pub const PI_ON: i32 = 1;
-pub const PI_OFF: i32 = 0;
+/// Values
+pub const PI_ON: u32 = 1;
+pub const PI_OFF: u32 = 0;
 
-type GpioResult = Result<(), String>;
-type GpioResponse = Result<i32, String>;
+/// Responses
+pub type GpioResult = Result<(), String>;
+pub type GpioResponse = Result<u32, String>;
 
 const DEFAULT_ERROR: &str = "Unknown error.";
 
-//Initializes the pigpio library. Must be called before other pigpio library functions.
-//Returns: Result<()>
+/// Initialises the library.
+/// gpioInitialise must be called before using the other library functions with some exceptions not yet wrapped.
 pub fn initialize() -> GpioResult {
     match unsafe { gpioInitialise() } {
         OK => Ok(()),
-        PI_INIT_FAILED => Err("Initializ failed".to_string()),
+        PI_INIT_FAILED => Err("Initialize failed".to_string()),
         _ => Err(DEFAULT_ERROR.to_string()),
     }
 }
 
-//Terminates the pigpio library.
+/// Terminates the library.
+/// Call before program exit.
+/// This function resets the used DMA channels, releases memory, and terminates any running threads.
 pub fn terminate() {
     unsafe { gpioTerminate() };
 }
 
-//modes:
-//PI_INPUT, PI_OUTPUT, ALT(0-5)
+/// Sets the GPIO mode, typically input or output.
 pub fn set_mode(gpio: u32, mode: u32) -> GpioResult {
     match unsafe { gpioSetMode(gpio, mode) } {
         OK => Ok(()),
@@ -217,23 +220,27 @@ pub fn set_mode(gpio: u32, mode: u32) -> GpioResult {
     }
 }
 
+/// Gets the GPIO mode.
 pub fn get_mode(gpio: u32) -> GpioResponse {
     let response = unsafe { gpioGetMode(gpio) };
     match response {
         PI_BAD_GPIO => Err("Bad gpio".to_string()),
-        _ => Ok(response),
+        _ => Ok(response as u32),
     }
 }
 
+/// Reads the GPIO level, on or off.
 pub fn read(gpio: u32) -> GpioResponse {
     match unsafe { gpioRead(gpio) } {
-        PI_ON => Ok(PI_ON),
-        PI_OFF => Ok(PI_OFF),
+        1 => Ok(PI_ON),
+        0 => Ok(PI_OFF),
         PI_BAD_GPIO => Err("Bad gpio".to_string()),
         _ => Err(DEFAULT_ERROR.to_string()),
     }
 }
 
+/// Sets the GPIO level, on or off.
+/// If PWM or servo pulses are active on the GPIO they are switched off.
 pub fn write(gpio: u32, level: u32) -> GpioResult {
     match unsafe { gpioWrite(gpio, level) } {
         OK => Ok(()),
@@ -243,6 +250,12 @@ pub fn write(gpio: u32, level: u32) -> GpioResult {
     }
 }
 
+/// Starts PWM on the GPIO, dutycycle between 0 (off) and range (fully on). Range defaults to 255.
+///
+/// This and the servo functionality use the DMA and PWM or PCM peripherals to control and schedule the pulse lengths and dutycycles.
+/// The 'set_pwm_range' function may be used to change the default range of 255.
+///
+/// The real value set by gpioPWM is (dutycycle * real range) / range.
 pub fn pwm(user_gpio: u32, dutycycle: u32) -> GpioResult {
     match unsafe { gpioPWM(user_gpio, dutycycle) } {
         OK => Ok(()),
@@ -252,17 +265,35 @@ pub fn pwm(user_gpio: u32, dutycycle: u32) -> GpioResult {
     }
 }
 
+/// Returns the PWM dutycycle setting for the GPIO.
+///
+/// For normal PWM the dutycycle will be out of the defined range for the GPIO (see 'get_pwm_range').
+/// If a hardware clock is active on the GPIO the reported dutycycle will be 500000 (500k) out of 1000000 (1M).
+/// If hardware PWM is active on the GPIO the reported dutycycle will be out of a 1000000 (1M).
+/// Normal PWM range defaults to 255.
 pub fn get_pwm_duty_cycle(user_gpio: u32) -> GpioResponse {
     let result = unsafe { gpioGetPWMdutycycle(user_gpio) };
     match result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
         PI_NOT_PWM_GPIO => Err("Not PWM gpio".to_string()),
-        _ => Ok(result),
+        _ => Ok(result as u32),
     }
 }
 
-pub fn servo(user_gpio: u32, pulsewidth: u32) -> GpioResult {
-    match unsafe { gpioServo(user_gpio, pulsewidth) } {
+/// Starts servo pulses on the GPIO, 0 (off), 500 (most anti-clockwise) to 2500 (most clockwise).
+/// The range supported by servos varies and should probably be determined by experiment. A value of 1500 should always be safe and represents the mid-point of rotation. You can DAMAGE a servo if you command it to move beyond its limits.
+/// The following causes an on pulse of 1500 microseconds duration to be transmitted on GPIO 17 at a rate of 50 times per second. This will command a servo connected to GPIO 17 to rotate to its mid-point.
+///
+/// This function updates servos at 50Hz. If you wish to use a different update frequency you will have to use the PWM functions.
+/// Firstly set the desired PWM frequency using 'set_pwm_frequency'.
+/// Then set the PWM range using set_pwm_range to 1E6/frequency. Doing this allows you to use units of microseconds when setting the servo pulsewidth.
+/// ~~~
+/// set_pwm_frequency(25, 400);
+/// set_pwm_range(25, 2500);
+/// ~~~
+/// Thereafter use the PWM command to move the servo, e.g. 'pwm(25, 1500)' will set a 1500 us pulse.
+pub fn servo(user_gpio: u32, pulse_width: u32) -> GpioResult {
+    match unsafe { gpioServo(user_gpio, pulse_width) } {
         OK => Ok(()),
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
         PI_BAD_PULSEWIDTH => Err("Bad pulse width".to_string()),
@@ -270,52 +301,88 @@ pub fn servo(user_gpio: u32, pulsewidth: u32) -> GpioResult {
     }
 }
 
-pub fn get_servo_pulse_width(user_gpio: u32) -> GpioResult {
-    match unsafe { gpioGetServoPulsewidth(user_gpio) } {
-        OK => Ok(()),
+/// Returns the servo pulsewidth setting for the GPIO.
+pub fn get_servo_pulse_width(user_gpio: u32) -> GpioResponse {
+    let result = unsafe { gpioGetServoPulsewidth(user_gpio) };
+    match  result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
         PI_NOT_SERIAL_GPIO => Err("Not server gpio".to_string()),
-        _ => Err(DEFAULT_ERROR.to_string()),
+        _ => Ok(result as u32),
     }
 }
 
+/// Delays for at least the number of microseconds specified by microseconds.
 pub fn delay(microseconds: u32) -> u32 {
     unsafe { gpioDelay(microseconds) }
 }
 
-pub fn set_pwm_range(user_gpio: u32, range: u32) -> GpioResult {
-    match unsafe { gpioSetPWMrange(user_gpio, range) } {
-        OK => Ok(()),
+/// Selects the dutycycle range to be used for the GPIO.
+/// Subsequent calls to pwm will use a dutycycle between 0 (off) and range (fully on).
+pub fn set_pwm_range(user_gpio: u32, range: u32) -> GpioResponse {
+    let result = unsafe { gpioSetPWMrange(user_gpio, range) };
+    match result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
         PI_BAD_DUTYRANGE => Err("Bad range".to_string()),
-        _ => Err(DEFAULT_ERROR.to_string()),
+        _ => Ok(result as u32)
     }
 }
 
+/// Returns the dutycycle range used for the GPIO.
+/// If a hardware clock or hardware PWM is active on the GPIO the reported range will be 1000000 (1M).
 pub fn get_pwm_range(user_gpio: u32) -> GpioResponse {
     let result = unsafe { gpioGetPWMrange(user_gpio) };
     match result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
-        _ => Ok(result),
+        _ => Ok(result as u32),
     }
 }
 
-pub fn set_pwm_frequency(user_gpio: u32, frequency: u32) -> GpioResult {
-    match unsafe { gpioSetPWMfrequency(user_gpio, frequency) } {
-        OK => Ok(()),
+/// Returns the real range used for the GPIO.
+/// If a hardware clock is active on the GPIO the reported real range will be 1000000 (1M).
+/// If hardware PWM is active on the GPIO the reported real range will be approximately 250M divided by the set PWM frequency.
+pub fn get_pwm_real_range(user_gpio: u32) -> GpioResponse {
+    let result = unsafe { gpioGetPWMrealRange(user_gpio) };
+    match result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
-        _ => Err(DEFAULT_ERROR.to_string()),
+        _ => Ok(result as u32),
     }
 }
 
+/// Sets the frequency in hertz to be used for the GPIO.
+pub fn set_pwm_frequency(user_gpio: u32, frequency: u32) -> GpioResponse {
+    let result = unsafe { gpioSetPWMfrequency(user_gpio, frequency) };
+    match result {
+        PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
+        _ => Ok(result as u32),
+    }
+}
+
+/// Returns the frequency in hertz used for the GPIO.
 pub fn get_pwm_frequency(user_gpio: u32) -> GpioResponse {
     let result = unsafe { gpioGetPWMfrequency(user_gpio) };
     match result {
         PI_BAD_USER_GPIO => Err("Bad user gpio".to_string()),
-        _ => Ok(result),
+        _ => Ok(result as u32),
     }
 }
 
+/// Starts a hardware clock on a GPIO at the specified frequency. Frequencies above 30MHz are unlikely to work.
+///
+/// The same clock is available on multiple GPIO. The latest frequency setting will be used by all GPIO which share a clock.
+/// The GPIO must be one of the following:
+/// 4   clock 0  All models
+/// 5   clock 1  All models but A and B (reserved for system use)
+/// 6   clock 2  All models but A and B
+/// 20  clock 0  All models but A and B
+/// 21  clock 1  All models but A and Rev.2 B (reserved for system use)
+///
+/// 32  clock 0  Compute module only
+/// 34  clock 0  Compute module only
+/// 42  clock 1  Compute module only (reserved for system use)
+/// 43  clock 2  Compute module only
+/// 44  clock 1  Compute module only (reserved for system use)
+///
+/// Access to clock 1 is protected by a password as its use will likely crash the Pi. The password is given by or'ing 0x5A000000 with the GPIO number.
 pub fn hardware_clock(gpio: u32, frequency: u32) -> GpioResult {
     match unsafe { gpioHardwareClock(gpio, frequency) } {
         OK => Ok(()),
@@ -327,6 +394,26 @@ pub fn hardware_clock(gpio: u32, frequency: u32) -> GpioResult {
     }
 }
 
+
+/// Starts hardware PWM on a GPIO at the specified frequency and dutycycle.
+/// Frequencies above 30MHz are unlikely to work.
+///
+//NOTE: Any waveform started by gpioWaveTxSend, or gpioWaveChain will be cancelled.
+// This function is only valid if the pigpio main clock is PCM. The main clock defaults to PCM but may be overridden by a call to gpioCfgClock.
+///
+/// The same PWM channel is available on multiple GPIO. The latest frequency and dutycycle setting will be used by all GPIO which share a PWM channel.
+/// The GPIO must be one of the following:
+///
+/// 12  PWM channel 0  All models but A and B
+/// 13  PWM channel 1  All models but A and B
+/// 18  PWM channel 0  All models
+/// 19  PWM channel 1  All models but A and B
+///
+/// 40  PWM channel 0  Compute module only
+/// 41  PWM channel 1  Compute module only
+/// 45  PWM channel 1  Compute module only
+/// 52  PWM channel 0  Compute module only
+/// 53  PWM channel 1  Compute module only
 pub fn hardware_pwm(gpio: u32, frequency: u32, dutycycle: u32) -> GpioResult {
     match unsafe { gpioHardwarePWM(gpio, frequency, dutycycle) } {
         OK => Ok(()),
