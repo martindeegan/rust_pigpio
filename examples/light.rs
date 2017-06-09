531c7e43c5dc4a8148d9ea71ce1ce0b6ddda86db
Martin Deegan <mddeegan@gmail.com>
2017-06-09 14:40:29 -0700

    Restructured

--- examples/light.rs
+++ examples/light.rs
@@ -1,7 +1,7 @@
 extern crate rust_pigpio;
 
 use std::thread::sleep;
-use rust_pigpio::pigpio::*;
+use rust_pigpio::pwm::*;
 
 const PIN: u32 = 21;
 
--- examples/pwm.rs
+++ examples/pwm.rs
@@ -2,7 +2,7 @@ extern crate rust_pigpio;
 
 use std::thread::sleep;
 use std::time::Duration;
-use rust_pigpio::pigpio::*;
+use rust_pigpio::pwm::*;
 
 const PIN: u32 = 21;
 
