f83e7e580c0b1ba564ff81b6d99bbc8c83f2793b
StefanSchoof <Stefan.Schoof@direkt-gruppe.de>
2017-11-14 21:05:50 +0100

    convert pwm to pigpio struct

--- examples/pwm.rs
+++ examples/pwm.rs
@@ -3,7 +3,6 @@ extern crate rust_pigpio;
 use std::thread::sleep;
 use std::time::Duration;
 use rust_pigpio::*;
-use rust_pigpio::pwm::*;
 
 const PIN: u32 = 21;
 
@@ -15,30 +14,30 @@ fn main() {
     pigpio.write(PIN, OFF).unwrap();
     println!("Light off.");
 
-    set_pwm_frequency(PIN, 500).unwrap(); // Set to modulate at 500hz.
-    set_pwm_range(PIN, 1000).unwrap(); // Set range to 1000. 1 range = 2 us;
+    pigpio.set_pwm_frequency(PIN, 500).unwrap(); // Set to modulate at 500hz.
+    pigpio.set_pwm_range(PIN, 1000).unwrap(); // Set range to 1000. 1 range = 2 us;
 
-    pwm(PIN, 100).unwrap();
+    pigpio.pwm(PIN, 100).unwrap();
     println!("10%");
 
     sleep(Duration::from_secs(2));
-    pwm(PIN, 250).unwrap();
+    pigpio.pwm(PIN, 250).unwrap();
     println!("25%");
 
     sleep(Duration::from_secs(2));
-    pwm(PIN, 500).unwrap();
+    pigpio.pwm(PIN, 500).unwrap();
     println!("50%");
 
     sleep(Duration::from_secs(2));
-    pwm(PIN, 735).unwrap();
+    pigpio.pwm(PIN, 735).unwrap();
     println!("73.5%");
 
     sleep(Duration::from_secs(2));
-    pwm(PIN, 50).unwrap();
+    pigpio.pwm(PIN, 50).unwrap();
     println!("5%");
 
     sleep(Duration::from_secs(2));
-    pwm(PIN, 1000).unwrap();
+    pigpio.pwm(PIN, 1000).unwrap();
     println!("100%");
 
     sleep(Duration::from_secs(2));
